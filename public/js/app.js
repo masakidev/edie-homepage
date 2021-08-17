cbeefd9a897c8dd3fb0fa29e081ebb89fb2716eb
masakidev <masakideveloper@gmail.com>
2021-08-17 14:24:18 -0500

    web and mobile version complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,16 @@
-const $navbarToggle = document.getElementById('navbar__toggle');
+const $header = document.getElementById('header');
+const $logo = document.getElementById('logo');
 const $navbar = document.getElementById('navbar');
+const $navbarToggle = document.getElementById('navbar__toggle');
 
 $navbarToggle.addEventListener('click', () => {
-  $navbar.classList.toggle('navbar__is-active')
+  const setToggle = $navbar.classList.toggle('navbar--is-active');
+  $header.classList.toggle('header--is-active');
+  $logo.classList.toggle('logo--is-active');
+  $navbarToggle.classList.toggle('navbar__toggle--is-active');
+  if (setToggle) {
+    $navbarToggle.textContent = 'close';
+  } else {
+    $navbarToggle.textContent = 'menu';
+  }
 })
